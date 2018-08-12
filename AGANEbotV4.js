dab769747ae52cc8ae53be36238d2f25c9ea8bf6
zEjoxx <41965704+zEjoxx@users.noreply.github.com>
2018-08-12 22:28:19 +0300

    Update AGANEbotV4.js

--- AGANEbotV4.js
+++ AGANEbotV4.js
@@ -73,7 +73,7 @@
         historySkip: true,
         timeGuard: true,
         strictTimeGuard: false,
-        maximumSongLength: 5.5,
+        maximumSongLength: 8,
         autodisable: false,
         commandCooldown: 30,
         usercommandsEnabled: false,
